72496c2fe300b197ac31bcc7f91560b08ff5ea66
Vinicius Nepomuceno <vi.github@outlook.com>
2024-10-01 23:31:22 -0300

    add

--- auth/main.go
+++ auth/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
+	"database/sql"
+	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/feature/rds/auth"
 	_ "github.com/lib/pq"
 	"log"
 	"os"
@@ -31,47 +36,43 @@ func Handler(request events.APIGatewayCustomAuthorizerRequest) (Response, error)
 		return returnPolice("Allow", request.MethodArn)
 	}
 
-	//var dbName = os.Getenv("DB_NAME")
-	//var dbUser = os.Getenv("DB_USER")
-	//var dbHost = os.Getenv("DB_HOST")
-	//var dbPort = 5432
-	//var dbEndpoint = fmt.Sprintf("%s:%d", dbHost, dbPort)
-	//var region = os.Getenv("AWS_REGION")
-	//
-	//cfg, err := config.LoadDefaultConfig(context.TODO())
-	//if err != nil {
-	//	panic("configuration error: " + err.Error())
-	//}
-	//
-	//authenticationToken, err := auth.BuildAuthToken(
-	//	context.TODO(), dbEndpoint, region, dbUser, cfg.Credentials)
-	//if err != nil {
-	//	panic("failed to create authentication token: " + err.Error())
-	//}
-	//
-	//dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?tls=true&allowCleartextPasswords=true",
-	//	dbUser, authenticationToken, dbEndpoint, dbName,
-	//)
-	//
-	//db, err := sql.Open("postgres", dsn)
-	//if err != nil {
-	//	panic(err)
-	//}
-	//defer db.Close()
+	var dbName = os.Getenv("DB_NAME")
+	var dbUser = os.Getenv("DB_USER")
+	var dbHost = os.Getenv("DB_HOST")
+	var dbPort = 5432
+	var dbEndpoint = fmt.Sprintf("%s:%d", dbHost, dbPort)
+	var region = os.Getenv("AWS_REGION")
 
-	//var count int
-	//err = db.QueryRow("SELECT COUNT(*) FROM cliente WHERE cpf = $1", cpf).Scan(&count)
-	//if err != nil {
-	//	log.Print("The client doest exists")
-	//	effect = "Deny"
-	//}
-
-	if request.AuthorizationToken == "12345678900" {
-		return returnPolice("Allow", request.MethodArn)
-		log.Print("The client exists")
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		panic("configuration error: " + err.Error())
 	}
 
-	return returnPolice("Deny", request.MethodArn)
+	authenticationToken, err := auth.BuildAuthToken(
+		context.TODO(), dbEndpoint, region, dbUser, cfg.Credentials)
+	if err != nil {
+		panic("failed to create authentication token: " + err.Error())
+	}
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?tls=true&allowCleartextPasswords=true",
+		dbUser, authenticationToken, dbEndpoint, dbName,
+	)
+
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	var count int
+	err = db.QueryRow("SELECT COUNT(*) FROM cliente WHERE cpf=$1", cpf).Scan(&count)
+	if err != nil {
+		log.Print("The client doest exists")
+		return returnPolice("Deny", request.MethodArn)
+	}
+
+	log.Print("The client exists")
+	return returnPolice("Allow", request.MethodArn)
 }
 
 func returnPolice(effect, methodArn string) (Response, error) {
